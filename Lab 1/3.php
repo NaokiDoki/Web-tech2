bc2a2ecf1fec83fc949886d4b23b1b1e5d3e1a62
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2023-11-27 07:30:28 +0545

    Add files via upload

---
+++ Lab 1/3.php
@@ -0,0 +1,15 @@
+<html>
+	<body>
+	<?php
+		$num = 10;
+		if($num % 2 == 0){
+			echo "$num is even";
+		}	
+		else{	
+			echo "$num is Odd";
+		}
+
+	?>
+	</body>
+
+</html>
---
+++ Lab 1/Lab1.php
@@ -0,0 +1,12 @@
+<html>
+	<body>
+	<?php
+		$num = 10;
+		if($num % 2 == 0){
+			echo "the number is even";
+		}	
+
+	?>
+	</body>
+
+</html>

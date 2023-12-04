49e39a870dedbc859203bdfcfb5cd99389bbd886
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2023-12-04 07:24:10 +0545

    Update 7.php

--- Lab 1/7.php
+++ Lab 1/7.php
@@ -4,7 +4,7 @@
         $month = 5;
         $year = 2003;
         $days = cal_days_in_month(CAL_GREGORIAN, $month, $year);
-        echo "There are $days days in month $month of year $year";
+        echo "There are $days days in $month, $year";
   ?>
   </body>
 </html>

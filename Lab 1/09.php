6e189896c37818daf2d90b4e4ac1c17e2383ed8e
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2023-12-04 07:29:02 +0545

    Update 09.php

--- Lab 1/09.php
+++ Lab 1/09.php
@@ -1,1 +1,14 @@
-
+<html>
+  <body>
+    <?php
+        $num = 45421;
+        $rev = 0;
+        while ($num > 0) {
+            $digit = $num % 10;
+            $reversed = $rev * 10 + $digit;
+            $num = (int)($num / 10);
+        }
+        echo "Reversed = $rev";
+    ?>
+  </body>
+</html>

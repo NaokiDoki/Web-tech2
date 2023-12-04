a01262fd1761cbe0b56767114435d96e592247d9
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2023-12-04 07:27:55 +0545

    Update 08.php

--- Lab 1/08.php
+++ Lab 1/08.php
@@ -1,1 +1,19 @@
-
+<html>
+  <body>
+    <?php
+        $num = 153;
+        $temp = $num;
+        $sum = 0;
+        while ($temp != 0) {
+            $remainder = $temp % 10;
+            $sum += $remainder ** 3;
+            $temp = (int)($temp / 10);
+        }
+        if ($sum == $num) {
+            echo "$num is an Armstrong number";
+        } else {
+            echo "$num is not an Armstrong number";
+        }
+    ?>
+  </body>
+</html>

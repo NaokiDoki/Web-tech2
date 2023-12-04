6889eb6219bdde07f063d095a46128cdb8e05398
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2023-12-04 07:29:33 +0545

    Update 08.php

--- Lab 1/08.php
+++ Lab 1/08.php
@@ -5,8 +5,8 @@
         $temp = $num;
         $sum = 0;
         while ($temp != 0) {
-            $remainder = $temp % 10;
-            $sum += $remainder ** 3;
+            $rem = $temp % 10;
+            $sum += $rem ** 3;
             $temp = (int)($temp / 10);
         }
         if ($sum == $num) {

b9f22d5e2559836bbc77c6fc1ff8ec18e2385f28
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2023-12-04 12:04:42 +0545

    Update 09.php

--- Lab 1/09.php
+++ Lab 1/09.php
@@ -5,7 +5,7 @@
         $rev = 0;
         while ($num > 0) {
             $digit = $num % 10;
-            $reversed = $rev * 10 + $digit;
+            $rev = $rev * 10 + $digit;
             $num = (int)($num / 10);
         }
         echo "Reversed = $rev";

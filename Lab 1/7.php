24421189532d6ea7b664d2caef427f4720fc9dfd
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2023-12-04 07:10:19 +0545

    Update 7.php

--- Lab 1/7.php
+++ Lab 1/7.php
@@ -1,1 +1,10 @@
-
+<htlm>
+  <body> 
+  <?php
+        $month = 2;
+        $year = 2023;
+        $days = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+        echo "There are $days days in month $month of year $year";
+  ?>
+  </body>
+</htlm>
